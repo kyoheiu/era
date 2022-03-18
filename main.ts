3ed0ef0d8e765c4199e2a35f0a6c9c45a1d954a6
Kyohei Uto <kyoheiu@outlook.com>
2022-03-18 11:28:50 +0900

    v0.1.0

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
+const ATOM1 = "█";
+const ATOM2 = "■";
+
+const TIME_WIDTH = 25;
+const TIME_HEIGHT = 5;
+
+const read_env_number = (name: string): number => {
+  const env = Deno.env.get(name);
+  if (env === null) {
+    return 100;
+  } else {
+    return parseInt(env!);
+  }
+};
+
+const read_env_str = (name: string): string => {
+  const env = Deno.env.get(name);
+  if (env === null) {
+    return "│";
+  } else {
+    return env!;
+  }
+};
+
+const INTERVAL = read_env_number("ERA_INTERVAL");
+const FREQUENCY = read_env_number("ERA_FREQUENCY");
+const RAIN1 = read_env_str("ERA_RAIN1");
+const RAIN2 = read_env_str("ERA_RAIN2");
+
 const ONE = [
   [0, 0, 1, 1, 0],
   [0, 0, 1, 1, 0],
@@ -86,19 +115,15 @@ const COLON = [
   [0, 0, 0],
 ];
 
-const TIME_WIDTH = 25;
-const TIME_HEIGHT = 5;
-const INTERVAL = 500;
-
 const generate_string_array = (num: number[][]): string[] => {
   const result: string[] = [];
   num.forEach((nums) => {
     let line = "";
     nums.forEach((num) => {
       if (num == 1) {
-        line = line + "█";
+        line = line + ATOM1;
       } else if (num == 2) {
-        line = line + "■";
+        line = line + ATOM2;
       } else {
         line = line + " ";
       }
@@ -156,16 +181,15 @@ const num_to_arrays = (num: number): number[][] => {
 };
 
 const call_rain = (rain: string[], column: number, row: number): string[] => {
-  if (rain.length <= row) {
-    let new_rain = "";
-    for (let i = 0; i < column; i++) {
-      new_rain = new_rain + make_drop(getRandomInt(4));
-    }
-    rain = [new_rain, ...rain];
-    return rain;
-  } else {
-    return rain;
+  if (rain.length >= row) {
+    rain.pop();
   }
+  let new_rain = "";
+  for (let i = 0; i < column; i++) {
+    new_rain = new_rain + make_drop(getRandomInt(FREQUENCY));
+  }
+  rain = [new_rain, ...rain];
+  return rain;
 };
 
 const getRandomInt = (max: number): number => {
@@ -173,47 +197,53 @@ const getRandomInt = (max: number): number => {
 };
 
 const make_drop = (rand: number): string => {
-  if (rand == 0) {
-    return "│";
-  } else {
-    return " ";
+  switch (rand) {
+    case 0:
+      return RAIN1;
+    case 1:
+      return RAIN2;
+    default:
+      return " ";
   }
 };
 
 const main = async () => {
   const { columns, rows } = Deno.consoleSize(Deno.stdout.rid);
-  const start_x = Math.floor((columns - TIME_WIDTH) / 2) - 1;
-  const start_y = Math.floor((rows - TIME_HEIGHT) / 2) - 1;
+  const start_x = Math.floor((columns - TIME_WIDTH) / 2);
+  const start_y = Math.floor((rows - TIME_HEIGHT) / 2);
   Deno.setRaw(Deno.stdin.rid, true); //Enter raw mode
   const c = new Uint8Array(1);
   await Deno.stdout.write(new TextEncoder().encode("\x1b[?1049h")); //Enter new screen
   await Deno.stdout.write(new TextEncoder().encode("\x1b[?25l")); //Hide cursor
   for (let i = 0; i < 5; i++) {
     const txt = generate_string_array(concat_nums(make_time()));
-    const move =
-      "\x1b[" + (start_y + i).toString() + ";" + start_x.toString() + "f";
+    const move = "\x1b[" + (start_y + i).toString() + ";" + start_x.toString() +
+      "f";
     await Deno.stdout.write(new TextEncoder().encode(move)); //Go to home position
     console.log(txt[i]);
   }
   let rain: string[] = [];
-  const intervalID = setInterval(async () => {
+  const interval_rainID = setInterval(async () => {
     await Deno.stdout.write(new TextEncoder().encode("\x1b[1;1f")); //Go to home position
     rain = call_rain(rain, columns, rows);
     for (let i = 1; i < rain.length; i++) {
+      if (i >= start_y && i < start_y + 5) {
+        continue;
+      }
       const move = "\x1b[" + i.toString() + ";" + "1f";
       await Deno.stdout.write(new TextEncoder().encode(move)); //Go to each rain-start position
       console.log(rain[i]);
     }
     for (let i = 0; i < 5; i++) {
       const txt = generate_string_array(concat_nums(make_time()));
-      const move =
-        "\x1b[" + (start_y + i).toString() + ";" + start_x.toString() + "f";
+      const move = "\x1b[" + (start_y + i).toString() + ";" +
+        start_x.toString() + "f";
       await Deno.stdout.write(new TextEncoder().encode(move)); //Go to time-start position
       console.log(txt[i]);
     }
   }, INTERVAL);
   await Deno.stdin.read(c);
-  clearInterval(intervalID);
+  clearInterval(interval_rainID);
   await Deno.stdout.write(new TextEncoder().encode("\x1b[?25h")); //Show cursor
   await Deno.stdout.write(new TextEncoder().encode("\x1b[?1049l")); //Restore main screen
   Deno.setRaw(Deno.stdin.rid, false); //Exit raw mode
